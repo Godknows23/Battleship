2d43d9026633779cf3574bc817b767e869a72396
Godknows@23 <godknows@velocityinc.tech>
2023-02-06 14:12:06 +0200

    Adding more test suites

--- src/All.test.js
+++ src/All.test.js
@@ -9,3 +9,25 @@ describe("ship", () => {
     const admiral = new Ship(length);
     expect(admiral.getShipLength()).toBe(3);
   });
+  test("ship should have an array of objects with hit property", () => {
+    const admiral = new Ship(2);
+    const object = [{ hit: false }, { hit: false }];
+    expect(admiral.getShip()).toEqual(object);
+  });
+  test("ship should be hittable", () => {
+    const admiral = new Ship(3);
+    admiral.hit(1);
+    expect(admiral.getShip()[1]).toEqual({ hit: true });
+  });
+  test("ship should be sinkable", () => {
+    const admiral = new Ship(1);
+    admiral.hit(0);
+    expect(admiral.isSunk()).toBe(true);
+  });
+  test("gameboard returns 2d array", () => {
+    const board = new GameBoard();
+    expect(board.getGameBoard().length).toBe(10);
+    expect(board.getGameBoard()[0].length).toBe(10);
+
+  });
+});
